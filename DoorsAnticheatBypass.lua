c0979d6195adb56ba3d6e2f9acc6cf1a0b8a4add
THEGUYSthataresop <104650253+THEGUYSthataresop@users.noreply.github.com>
2023-02-02 20:10:35 +0100

    Update DoorsAnticheatBypass.lua

--- DoorsAnticheatBypass.lua
+++ DoorsAnticheatBypass.lua
@@ -11,16 +11,29 @@ YEYYY
 
 
 
-local player = game.Players.LocalPlayer
-local HRP = player.Character:WaitForChild("HumanoidRootPart")
 
-if true then
-  state = HRP.Parent.Humanoid:GetState()
-end
 
-if state then
-  print("Success")
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 for i = 1, 2 do
   game.GuiService:ToggleFullscreen()
